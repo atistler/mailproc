741f3656b9299078839972197e8ea75f016ef8e6
atistler@gmail.com <atistler@macbook.(none)>
2011-10-20 00:35:03 -0400

    sr handling

--- sql/selectConnectionByAll.sql
+++ sql/selectConnectionByAll.sql
@@ -3,3 +3,4 @@ FROM    connections
 WHERE   connector_node_id = :connection.connectorId
 AND     connectee_node_id = :connection.connecteeId
 AND     connection_type_id = :connection.connectionTypeId
+LIMIT   1
